12113ba31f66594bca328d592ad1a3a12ac267b0
cconley <conley.christopher@gmail.com>
2017-04-06 18:51:45 -0700

    MAJOR cross-validation function changes.

--- R/cvVoteRDS.R
+++ R/cvVoteRDS.R
@@ -1,14 +1,23 @@
-cvVoteRDS <- function(mod_lambda, tol, major_thresh = 0.5, 
-                   method = c("spacemap", "space", "scggm")) { 
-  fold_fits <- lapply(mod_lambda, readRDS)
+cvVoteRDS <- function(files, tol, thresh = 0.5, 
+                   method = c("spacemap", "space", "scggm"), 
+                   givenX = TRUE) { 
+  method <- match.arg(method)
+  fold_fits <- lapply(files, readRDS)
   npotential_fits <- length(fold_fits)
   nfits <- 0
-  p <- nrow(as.matrix(fold_fits[[1]]$ThetaXY))
-  q <- ncol(as.matrix(fold_fits[[1]]$ThetaXY))
-  vote_xy <- matrix(0, nrow = p, ncol = q)
+  
+  #vanilla space (no conditioning on x)
+  part_xy <- !(method == "space" & !givenX)
+
+  q <- ncol(as.matrix(fold_fits[[1]]$ThetaYY))
   vote_yy <- matrix(0, nrow = q, ncol = q)
-  deg_xy <- matrix(0, nrow = npotential_fits, ncol = p)
   deg_yy <- matrix(0, nrow = npotential_fits, ncol = q)
+  if(part_xy) { 
+    p <- nrow(as.matrix(fold_fits[[1]]$ThetaXY))
+    vote_xy <- matrix(0, nrow = p, ncol = q)
+    deg_xy <- matrix(0, nrow = npotential_fits, ncol = p)
+  }
+  
   for (i in seq_along(fold_fits)) {
     if (method == "scggm") { 
       valid <- TRUE
@@ -19,22 +28,33 @@ cvVoteRDS <- function(mod_lambda, tol, major_thresh = 0.5,
     if (!valid) { 
       next;
     }
-    xy <- abs(as.matrix(fold_fits[[i]]$ThetaXY)) > tol
+    
     yy <- abs(as.matrix(fold_fits[[i]]$ThetaYY)) > tol
-    deg_xy[i,] <- rowSums(xy)
     deg_yy[i,] <- rowSums(yy)
     nfits <- nfits + 1
-    vote_xy <- vote_xy + xy
     vote_yy <- vote_yy + yy
+    
+    if (part_xy) { 
+      xy <- abs(as.matrix(fold_fits[[i]]$ThetaXY)) > tol
+      deg_xy[i,] <- rowSums(xy)
+      vote_xy <- vote_xy + xy
+    }
   }
   
-  if (nfits < ceiling(major_thresh*length(fold_fits))) { 
+  if (nfits < ceiling(thresh*length(fold_fits))) { 
     return(NA)
   } else { 
-    cutoff <- floor(major_thresh*nfits)
-    vote_xy <- matrix(as.numeric(vote_xy > cutoff), nrow = p, ncol = q)
-    vote_yy <- matrix(as.numeric(vote_yy > cutoff), nrow = q, ncol = q)
+    cutoff <- floor(thresh*nfits)
+    vote_yy <- (vote_yy > cutoff) + 0
+    if (part_xy) { 
+      vote_xy <- (vote_xy > cutoff) + 0
+    }
   }
-  list(xy = vote_xy, yy = vote_yy, nfits = nfits, deg_xy = deg_xy, deg_yy = deg_yy)
+  if (part_xy) { 
+    ret <- list(xy = vote_xy, yy = vote_yy, nfits = nfits, deg_xy = deg_xy, deg_yy = deg_yy)
+  } else { 
+    ret <- list(yy = vote_yy, nfits = nfits, deg_yy = deg_yy)
+  }
+  ret
 }
 
